25cb873fb28fa783cb8f7166fcb243ab9f5c7c87
Travis Martin <tjmartin2003@gmail.com>
2024-12-18 23:45:04 -0600

    Bump to retry GitHub Actions.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
 import auth from "../../../../scripts/auth";
 
-// TODO: Custom sign in pages? See https://authjs.dev/getting-started/session-management/custom-pages.
-
 /**
  * The HTTP request handlers. Necessary for NextAuth.js to work.
  * @public
