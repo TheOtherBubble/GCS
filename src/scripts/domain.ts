cb6e1842ce32e4f9e271242ffd5f8ab6f535e268
Travis Martin <tjmartin2003@gmail.com>
2024-12-24 15:57:04 -0600

    New domain.

--- src/scripts/domain.ts
+++ src/scripts/domain.ts
@@ -5,4 +5,4 @@
 export default process.env["VERCEL_ENV"] === "development" ||
 process.env.NODE_ENV === "development"
 	? `http://localhost:${process.env["PORT"] ?? "3000"}`
-	: "https://vln.lakuna.pw"; // TODO: Update once domain is set up.
+	: "https://www.gcsleague.com/";
