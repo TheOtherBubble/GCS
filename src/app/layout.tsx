cf1380bb4b6a96979f3e79d5976c3c221860583f
Travis Martin <tjmartin2003@gmail.com>
2025-02-16 20:39:14 -0600

    Move topnav into base layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import "styles/global.scss";
 import type { Metadata, Viewport } from "next";
+import { auth, signIn, signOut } from "db/auth";
 import { body, title } from "util/font";
+import Form from "components/Form";
+import Icon from "./icon.svg";
 import type { JSX } from "react";
 import type LayoutProps from "types/LayoutProps";
-import Topnav from "components/Topnav";
+import Link from "components/Link";
+import Submit from "components/Submit";
 import domain from "util/domain";
+import getPlayerUrl from "util/getPlayerUrl";
 import multiclass from "util/multiclass";
 import style from "./layout.module.scss";
 
@@ -14,12 +19,78 @@ import style from "./layout.module.scss";
  * @returns The root layout.
  * @public
  */
-export default function Layout({ children }: LayoutProps): JSX.Element {
+export default async function Layout({
+	children
+}: LayoutProps): Promise<JSX.Element> {
+	const session = await auth();
+
 	return (
 		<html lang="en-US" className={multiclass(title.variable, body.variable)}>
 			<body className={style["spacer"]}>
 				<header>
-					<Topnav />
+					<nav>
+						<ul>
+							<li>
+								<Link href="/">
+									<Icon />
+								</Link>
+							</li>
+							<li>
+								<Link href="/schedule">
+									<span>{"Schedule"}</span>
+								</Link>
+							</li>
+							<li className={style["hide-on-mobile"]}>
+								<Link href="/rulebook">
+									<span>{"Rulebook"}</span>
+								</Link>
+							</li>
+							<li className={style["hide-on-medium"]}>
+								<Link href="/leaderboards">
+									<span>{"Leaderboards"}</span>
+								</Link>
+							</li>
+							{session?.user ? (
+								<>
+									<li className={style["right"]}>
+										<Link href={getPlayerUrl(session.user)}>
+											<span>
+												{session.user.displayName ?? session.user.name}
+											</span>
+										</Link>
+									</li>
+									{session.user.isAdministator && (
+										<li className={style["hide-on-mobile"]}>
+											<Link href="/admin">
+												<span>{"Admin"}</span>
+											</Link>
+										</li>
+									)}
+									<li className={style["hide-on-mobile"]}>
+										<Form
+											action={async () => {
+												"use server";
+												await signOut();
+											}}
+										>
+											<Submit value="Sign Out" />
+										</Form>
+									</li>
+								</>
+							) : (
+								<li className={style["right"]}>
+									<Form
+										action={async () => {
+											"use server";
+											await signIn("discord");
+										}}
+									>
+										<Submit value="Sign In" />
+									</Form>
+								</li>
+							)}
+						</ul>
+					</nav>
 				</header>
 				<main>{children}</main>
 				<footer></footer>
