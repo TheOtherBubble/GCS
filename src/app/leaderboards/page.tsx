89eb1d10be24b98f2f78f96976dca37d0240caa1
Travis Martin <tjmartin2003@gmail.com>
2025-02-16 21:58:40 -0600

    More leaderboards.

--- src/app/leaderboards/page.tsx
+++ src/app/leaderboards/page.tsx
@@ -166,6 +166,102 @@ export default async function Page(): Promise<JSX.Element> {
 				</div>
 				<div>
 					<header>
+						<h2>{"Kills (Total)"}</h2>
+					</header>
+					<ol>
+						{resultsByPlayer
+							.map(({ value: player, children: results }) => ({
+								kills: results.reduce((total, { kills }) => total + kills, 0),
+								player
+							}))
+							.sort(({ kills: a }, { kills: b }) => b - a)
+							.slice(0, 10)
+							.map(({ kills, player }) => (
+								<li key={player.id}>
+									<Link href={getPlayerUrl(player)}>
+										{player.displayName ?? player.name}
+									</Link>
+									{` - ${kills.toLocaleString()}`}
+								</li>
+							))}
+					</ol>
+				</div>
+				<div>
+					<header>
+						<h2>{"Kills (Per Game)"}</h2>
+					</header>
+					<ol>
+						{resultsByPlayer
+							.map(({ value: player, children: results }) => ({
+								games: results.length,
+								kills: results.reduce((total, { kills }) => total + kills, 0),
+								player
+							}))
+							.sort((a, b) => b.kills / b.games - a.kills / a.games)
+							.slice(0, 10)
+							.map(({ kills, games, player }) => (
+								<li key={player.id}>
+									<Link href={getPlayerUrl(player)}>
+										{player.displayName ?? player.name}
+									</Link>
+									{` - ${(kills / games).toFixed(2)}`}
+								</li>
+							))}
+					</ol>
+				</div>
+				<div>
+					<header>
+						<h2>{"Kills (Single Game)"}</h2>
+					</header>
+					<ol>
+						{rows
+							.sort(
+								(
+									{ playerGameResult: { kills: a } },
+									{ playerGameResult: { kills: b } }
+								) => b - a
+							)
+							.slice(0, 10)
+							.map(({ game, player, playerGameResult }) => (
+								<li key={playerGameResult.id}>
+									<Link href={getPlayerUrl(player)}>
+										{player.displayName ?? player.name}
+									</Link>
+									{` - ${playerGameResult.kills.toString()} - `}
+									<Link
+										href={getGameUrl(game)}
+									>{`Game #${game.id.toString()}`}</Link>
+								</li>
+							))}
+					</ol>
+				</div>
+				<div>
+					<header>
+						<h2>{"Deaths (Total)"}</h2>
+					</header>
+					<ol>
+						{resultsByPlayer
+							.map(({ value: player, children: results }) => ({
+								deaths: results.reduce(
+									(total, { deaths }) => total + deaths,
+									0
+								),
+								player
+							}))
+							.sort(({ deaths: a }, { deaths: b }) => b - a)
+							.slice(0, 10)
+							.map(({ deaths, player }) => (
+								<li key={player.id}>
+									<Link href={getPlayerUrl(player)}>
+										{player.displayName ?? player.name}
+									</Link>
+									{` - ${deaths.toLocaleString()}`}
+								</li>
+							))}
+					</ol>
+				</div>
+				<div>
+					<header>
 						<h2>{"Deaths (Per Game)"}</h2>
 					</header>
 					<ol>
@@ -209,6 +305,83 @@ export default async function Page(): Promise<JSX.Element> {
 										{player.displayName ?? player.name}
 									</Link>
 									{` - ${playerGameResult.deaths.toString()} - `}
+									<Link
+										href={getGameUrl(game)}
+									>{`Game #${game.id.toString()}`}</Link>
+								</li>
+							))}
+					</ol>
+				</div>
+				<div>
+					<header>
+						<h2>{"Assists (Total)"}</h2>
+					</header>
+					<ol>
+						{resultsByPlayer
+							.map(({ value: player, children: results }) => ({
+								assists: results.reduce(
+									(total, { assists }) => total + assists,
+									0
+								),
+								player
+							}))
+							.sort(({ assists: a }, { assists: b }) => b - a)
+							.slice(0, 10)
+							.map(({ assists, player }) => (
+								<li key={player.id}>
+									<Link href={getPlayerUrl(player)}>
+										{player.displayName ?? player.name}
+									</Link>
+									{` - ${assists.toLocaleString()}`}
+								</li>
+							))}
+					</ol>
+				</div>
+				<div>
+					<header>
+						<h2>{"Assists (Per Game)"}</h2>
+					</header>
+					<ol>
+						{resultsByPlayer
+							.map(({ value: player, children: results }) => ({
+								assists: results.reduce(
+									(total, { assists }) => total + assists,
+									0
+								),
+								games: results.length,
+								player
+							}))
+							.sort((a, b) => b.assists / b.games - a.assists / a.games)
+							.slice(0, 10)
+							.map(({ assists, games, player }) => (
+								<li key={player.id}>
+									<Link href={getPlayerUrl(player)}>
+										{player.displayName ?? player.name}
+									</Link>
+									{` - ${(assists / games).toFixed(2)}`}
+								</li>
+							))}
+					</ol>
+				</div>
+				<div>
+					<header>
+						<h2>{"Assists (Single Game)"}</h2>
+					</header>
+					<ol>
+						{rows
+							.sort(
+								(
+									{ playerGameResult: { assists: a } },
+									{ playerGameResult: { assists: b } }
+								) => b - a
+							)
+							.slice(0, 10)
+							.map(({ game, player, playerGameResult }) => (
+								<li key={playerGameResult.id}>
+									<Link href={getPlayerUrl(player)}>
+										{player.displayName ?? player.name}
+									</Link>
+									{` - ${playerGameResult.assists.toString()} - `}
 									<Link
 										href={getGameUrl(game)}
 									>{`Game #${game.id.toString()}`}</Link>
