d5bc73d8fa6b5e95eade18f38548e4d1eb03ccee
Travis Martin <tjmartin2003@gmail.com>
2025-05-11 17:40:27 -0500

    Oopsie

--- src/app/seasons/[slug]/draft/DraftPlayerForm.tsx
+++ src/app/seasons/[slug]/draft/DraftPlayerForm.tsx
@@ -71,7 +71,6 @@ export default function DraftPlayerForm({
 						<Link href={ugg(...accounts)}>{"U.GG"}</Link>
 					</p>
 				)}
-				{draftPlayer.notes && <p>{`Notes: ${draftPlayer.notes}`}</p>}
 			</header>
 			<p>
 				<Submit value="Draft" disabled={!enabled} />
