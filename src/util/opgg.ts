a640ba2a92359a1123e54ef08063d7b8af19241c
TheOtherBubble <43484273+TheOtherBubble@users.noreply.github.com>
2025-05-31 17:01:55 -0500

    Create opgg.ts
    Signed-off-by: TheOtherBubble <43484273+TheOtherBubble@users.noreply.github.com>

---
+++ src/util/opgg.ts
@@ -0,0 +1,24 @@
+import type { accountTable } from "db/schema";
+
+/**
+ * Make a U.GG link for the given set of accounts.
+ * @param accounts - The accounts to include in the U.GG link.
+ * @returns A U.GG link.
+ * @public
+ */
+export default function opgg(
+	...accounts: Pick<
+		typeof accountTable.$inferSelect,
+		"name" | "tagLine" | "region"
+	>[]
+): string {
+	const [account] = accounts;
+	if (!account) {
+		return "https://u.gg/";
+	}
+
+	if (accounts.length <= 1) {
+		return `https://op.gg/lol/summoners/${account.region}/${encodeURIComponent(account.name)}-${encodeURIComponent(account.tagLine)}`;
+	}
+  return `https://op.gg/lol/multisearch/na?summoners=${accounts.map(({ name, tagLine }) => `${encodeURIComponent(name)}%23${encodeURIComponent(tagLine)}`).join(",")}&region=${account.region.toLowerCase()}`;
+}
